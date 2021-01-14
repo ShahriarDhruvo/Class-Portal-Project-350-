a9be697e9ce8dd3265a593091c6419a8eb81a70f
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2021-01-14 21:38:56 +0600

    clearing junks

--- frontend/src/components/generic/LoadingScreen.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../../styles/componentStyles/LoadingScreen.scss';
-
-const LoadingScreen = () => {
-    return <div className="spin"></div>;
-};
-
-export default LoadingScreen;
--- server/sections/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Section
-
-admin.site.register(Section)
